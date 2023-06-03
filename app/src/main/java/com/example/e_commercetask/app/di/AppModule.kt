363f84aab6af65949df4e5917863cff6cc5744f9
medhat <ahmed.medhat.sholkamy@gmail.com>
2023-06-03 17:05:35 +0300

    add project architecture

---
+++ app/src/main/java/com/example/e_commercetask/app/di/AppModule.kt
@@ -0,0 +1,50 @@
+package com.example.e_commercetask.app.di
+
+import com.example.e_commercetask.BuildConfig
+import com.example.e_commercetask.app.api.ApiService
+import com.example.e_commercetask.app.models.ErrorInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+        OkHttpClient.Builder()
+            .addInterceptor(ErrorInterceptor())
+            .addInterceptor(loggingInterceptor)
+            .build()
+    } else OkHttpClient
+        .Builder()
+        .addInterceptor(ErrorInterceptor())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService =
+        retrofit.create(ApiService::class.java)
+}
