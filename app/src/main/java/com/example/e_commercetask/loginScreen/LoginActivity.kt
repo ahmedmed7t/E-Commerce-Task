1e6a45300d625beb4b30c30571a168846454b9c9
medhat <ahmed.medhat.sholkamy@gmail.com>
2023-06-03 17:10:26 +0300

    add loginactivity

---
+++ app/src/main/java/com/example/e_commercetask/loginScreen/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e_commercetask.loginScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.e_commercetask.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
